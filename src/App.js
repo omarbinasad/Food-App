3eb97ae9f406ecb0d249861e1a21c17847c2eebd
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-03-31 01:17:39 +0600

    DeliveryLocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import DeliveryLocation from "./Pages/DeliveryLocation/DeliveryLocation";
 import BottomNavBar from "./Shared/BottomNavBar/BottomNavBar";
 import TopNavBar from "./Shared/TopNavBar/TopNavBar";
 
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/product-details" element={<ProductDetails />}></Route>
+          <Route path="/delivery-location" element={<DeliveryLocation/>}></Route>
+          
         </Routes>
         <BottomNavBar />
       </BrowserRouter>
